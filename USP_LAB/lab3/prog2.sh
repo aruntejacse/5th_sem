783b2d34ceacaa4f3b5780e589e91d141a52bdb3
aruntejacse <53857149+aruntejacse@users.noreply.github.com>
2020-10-08 15:20:14 +0530

    Add files via upload

--- USP_LAB/lab3/prog2.sh
+++ USP_LAB/lab3/prog2.sh
@@ -5,6 +5,26 @@
 #their attributes with suitable message if not display a suitable 
 #message to pass right number of arguments.
 
-read -p "Enter filename and attribute for file test:" fname attr
-
-  
+if [ $# -lt 2 ]
+then
+	echo "Enter two arguments"
+	exit 1
+elif [ $# -eq 2 ]
+then
+	if [ -f $1 ]
+	then
+		echo "File 1 arguments are:"
+		ls -l $1
+	else
+		echo "1st argument is not a file"
+	fi
+	if [ -f $2 ]
+	then
+		echo "File 2 arguments are:"
+		ls -l $2
+	else
+		echo "2nd argument is not a file"
+	fi
+else
+	echo "Something went wrong"
+fi 
